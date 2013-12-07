2b5a91d27b8f502f358793b5841efe08c56a8e95
Fronx <fronx@soundcloud.com>
2013-12-07 01:58:17 +0100

    minimal version of the game, without rules and scores

--- ishido.js
+++ ishido.js
@@ -1,56 +1,207 @@
-function Board () {
-  this.cells = [];
+var colors = require('colors');
+var Promise = require('promise');
+var readline = require('readline');
 
-  var y_max = 8;
-  var x_max = 12;
-  var n_fields = y_max * x_max;
+var Piece = function (color, symbol) {
+    this.color = color;
+    this.symbol = symbol;
 
-  function flatten_xy (x, y) {
-    return (y - 1) * x_max + (x - 1);
-  }
-
-  this.at = function (x, y) {
-    if ((x >= 1) && (x <= x_max) && (y >= 1) && (y <= y_max))
-      return this.cells[flatten_xy(x, y)]
-    else
-      return null;
-  }
-
-  this.set = function (x, y, value) {
-    this.cells[flatten_xy(x, y)] = value;
-  }
-
-  this.draw = function () {
-    for (i = 0; i < n_fields; i++) {
-      if ((i > 0) && (i % x_max == 0)) process.stdout.write("\n");
-      if (typeof this.cells[i] !== 'undefined')
-        process.stdout.write(this.cells[i])
-      else
-        process.stdout.write('☐');
+    this.toString = function () {
+        return this.symbol[this.color];
     }
-    process.stdout.write("\n");
-  }
-
-  this.neighbours = function (x, y) {
-    return [ this.at(x - 1, y)
-           , this.at(x + 1, y)
-           , this.at(x, y - 1)
-           , this.at(x, y + 1)
-           ].filter(function (cell) { return cell != null })
-  }
 }
 
-Colors = []
+var Board = function () {
+    board = this;
+    board.cells = [];
+    board.y_min = board.x_min = 1;
+    board.y_num = board.y_max = 8;
+    board.x_num = board.x_max = 12;
 
-b = new Board;
-b.set( 1, 1, 'a');
-b.set(12, 1, 'b');
-b.set( 1, 8, 'c');
-b.set(12, 8, 'd');
-b.set( 1, 2, 'x');
-b.draw();
+    var n_fields = board.y_num * board.x_num;
 
-console.log(b.neighbours(1,1))
-console.log(b.neighbours(1,2))
-console.log(b.neighbours(1,3))
-console.log(b.neighbours(1,4))
+    function flatten_xy (x, y) {
+        return (y - board.y_min) * board.x_num + (x - board.x_min);
+    }
+
+    board.valid_xy = function (x, y) {
+        return (x >= board.x_min) && (x <= board.x_max) && (y >= board.y_min) && (y <= board.y_max);
+    }
+
+    board.at = function (x, y) {
+        if (board.valid_xy(x, y))
+            return board.cells[flatten_xy(x, y)]
+        else
+            return null;
+    }
+
+    board.set = function (x, y, value) {
+        board.cells[flatten_xy(x, y)] = value;
+    }
+
+    board.draw = function () {
+        var empty_cell_string = '☐';
+        for (i = 0; i < n_fields; i++) {
+            if ((i > 0) && (i % board.x_num == 0)) process.stdout.write("\n");
+            if (board.cells[i] instanceof Piece)
+                process.stdout.write(board.cells[i].toString())
+            else
+                process.stdout.write(empty_cell_string);
+        }
+        process.stdout.write("\n");
+    }
+
+    board.neighbours = function (x, y) {
+        return [ board.at(x - 1, y)
+               , board.at(x + 1, y)
+               , board.at(x, y - 1)
+               , board.at(x, y + 1)
+               ].filter(function (cell) { return cell != null })
+    }
+}
+
+var Game = function () {
+    var game = this;
+    var turns = [];
+
+    var all_pieces = (function () {
+        var colors =
+            [ 'blue'
+            , 'cyan'
+            , 'green'
+            , 'magenta'
+            , 'red'
+            , 'yellow'
+            ]
+
+        var symbols =
+            [ 'a'
+            , 'b'
+            , 'c'
+            , 'd'
+            , 'e'
+            , 'f'
+            ]
+
+        var pieces = []
+        colors.forEach(function (color) {
+            symbols.forEach(function (symbol) {
+                pieces.push(new Piece(color, symbol));
+            })
+        })
+        return pieces.concat(pieces); // there's two of each
+    })();
+
+    /**
+     * Randomize array element order in-place.
+     * Using Fisher-Yates shuffle algorithm.
+     * http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+     */
+    function shuffle (array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }
+
+    var pieces = shuffle(all_pieces);
+    var board = new Board;
+
+    function put (x, y, piece) {
+        turns.push([ x, y, piece ]);
+        board.set(x, y, piece);
+    }
+    function put_initial_pieces () {
+        put(board.x_min, board.y_min, pieces.shift());
+        put(board.x_max, board.y_min, pieces.shift());
+        put(board.x_min, board.y_max, pieces.shift());
+        put(board.x_max, board.y_max, pieces.shift());
+    }
+
+    function free (x, y) {
+        return (!(board.at(x, y) instanceof Piece));
+    }
+    function verify_placing (x, y, piece) {
+        var valid = true;
+        board.neighbours(x, y).forEach(function (neighbour) {
+            // TODO
+        })
+        valid = valid && board.valid_xy(x, y)
+                      && free(x,y);
+        return valid;
+    }
+
+    game.completed = function () {
+        return (pieces.length == 0);
+    }
+    game.next = function (fn) {
+        var promise = new Promise(function (resolve, reject) {
+            var piece = pieces.shift();
+            fn(piece, board). // must return a promise
+                then(function (placing) {
+                    var x = placing.x;
+                    var y = placing.y;
+                    if (verify_placing(x, y, piece)) {
+                        put(x, y, piece);
+                        resolve(game);
+                    } else {
+                        pieces.unshift(piece);
+                        reject('invalid move');
+                    }
+                });
+        });
+        return promise;
+    }
+
+    put_initial_pieces();
+}
+
+var ask = function (prompt) {
+    var rl = readline.createInterface({
+        input:  process.stdin,
+        output: process.stdout
+    });
+    var promise = new Promise(function (resolve, reject) {
+        rl.question(prompt, function (answer) {
+            rl.close();
+            resolve(answer);
+        });
+
+    });
+    return promise;
+}
+
+var loop = function (game) {
+    if (!game.completed()) {
+        return game.next(user_turn).then(loop, function (message) {
+            console.log(message);
+            loop(game);
+        });
+    } else {
+        console.log('completed');
+    }
+}
+
+var user_turn = function (piece, board) {
+    process.stdout.write("\n");
+    board.draw();
+    console.log("\ncurrent piece: " + piece.toString());
+    return ask('x,y: ').then(function (input_string) {
+        xy = input_string.split(/\s*,\s*/);
+        return { x: parseInt(xy[0])
+               , y: parseInt(xy[1])
+               }
+    });
+}
+
+var main = function () {
+    game = new Game;
+    loop(game);
+}
+
+if (require.main === module) {
+    main();
+}
